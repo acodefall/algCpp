b7d5ec961b2036940ca5494a012fa9bd1c37cd5d
acodefall <asnowfall@hotmail.com>
2017-05-14 00:16:04 -0400

    modified files

--- algCpp/algCpp/algCpp/algCpp.cpp
+++ algCpp/algCpp/algCpp/algCpp.cpp
@@ -9,28 +9,121 @@
 #include "Graphbuild.h"
 #include "GraphSearch.h"
 #include "EditablePriorityQueue.h"
-#include "PriorityQUsage.h"
-#include "SetComparison.h"
-#include "MapComparison.h"
+
 #include "MatrixRotatePrint.h"
 #include "MovieRatingGraph.h"
+#include "Combinations.h"
+#include "Permutation.h"
+#include "MemoryMngrBuddy.h"
+#include "CombinationWithoutDupesBitGen.h"
+#include "PowerSet.h"
+#include "InvertedIndex.h"
+#include "Library.h"
+#include "Stack1.h"
+#include "Median.h"
+#include "SyncQx.h"
+#include "ProdConsumer.h"
+#include "AreStringRotated.h"
+#include "BinarySearchForLoop.h"
+#include "LCSString.h"
+#include "ArrayHasConsqNumbers.h"
+#include "SwapAlternativeKthNode.h"
+#include "PrintTopAndBottomFacingNodes.h"
+#include "SingleTonOne.h"
+#include "ReverseDoublyLinkedList.h"
+#include "RotateArray.h"
+#include "RotateArrayUsingReverse.h"
+#include "BoyerMooreVoting.h"
+#include "FindTopTenPercent.h"
+
 
 int main()
 {
+	LibraryNM::Library objLibrary;
+
+	cout << "main begins";
+	FindTopTenPercentNM::callFindTopTenPercentX objcallFindTopTenPercentX;
+	objcallFindTopTenPercentX.callFindTopTenPercent();
+
+	BoyerMooreVotingNM::BoyerMooreVotingX objBoyerMooreVotingX;
+	objBoyerMooreVotingX.callBoyerVoting();
+	
+	RotateArrayUsingReverseNM::RotateArrayUsingReverseX objRotateArrayUsingReverseX;
+	objRotateArrayUsingReverseX.callRotateArrayUsingReverse();
+
+	RotateArrayNM::RotateArrayX objRotateArray;
+	objRotateArray.callRotateArray();
+
+	ReverseDoublyLinkedListNM::ReverseDoublyLinkedListX objReverseDoublyLinkedListX;
+	objReverseDoublyLinkedListX.callRevDLL();
+
+	
+
+	testSingleTonOneX objtestSingleTonOneX;
+	objtestSingleTonOneX.callSingleTonOneX();
+
+
+	MedianOfMedian objMedianOfMedian;
+	objMedianOfMedian.call();
+
+
+
+	PrintTopAndBottomFacingNodes objPrintTopAndBottomFacingNodes;
+	objPrintTopAndBottomFacingNodes.callPrintTopAndBottomFacingNodes();
+
+	SwapKthNode<int> objSwapKthNode;
+	objSwapKthNode.callSwapKthNode();
+
+	ArrayHasConsecutiveNumber objArrayHasConsecutiveNumber;
+	objArrayHasConsecutiveNumber.callArrayHasConsecutiveNumber();
+
+	LCSstring objLCSstring;
+	objLCSstring.getLCS();
+
+	BinSearchForLoopNM::BinSearchForLoop objbinSearchForLoop;
+	objbinSearchForLoop.callBinSearchForLoop();
+
+	StringRotatedNM::StringRotated objStringRotated;
+	objStringRotated.callStringRotated();
+
+	StackTest xStackTest;
+	xStackTest.TestStack();
+
+
+	CallSyncDq objCallSyncDq;
+	objCallSyncDq.callSyncDqX();
+	objCallSyncDq.callSyncDqXSync();
+
+	callProdConsume objcallProdConsume;
+	objcallProdConsume.callProd();
+	////////////////////////////////////////////////
+	
+	TrieNM::UseInvertedIndex objUseInvertedIndex;
+	objUseInvertedIndex.CallUseInvertedIndex();
+
+	PowerSetNM::CallPowerSet objCallPowerSet;
+	objCallPowerSet.callPowerSetX();
+
+	CombinationsBitGenNM::CombinationUsingBitGen objCombinationUsingBitGen;
+	objCombinationUsingBitGen.useCombination();
+
+	BuddyMemoryManager::callAllocator callAllocatorX;
+	callAllocatorX.UseMemoryManager();
+		
+	PermutationsNM::Permutation objPerm;
+	objPerm.usePermutation();
+
+	CombinationsNM::Combination objComb1;
+	objComb1.useCombination();
+
 	GraphMovie::testGraphMv objtestGraphMv;
 	objtestGraphMv.testGraph();
 
 	MatrixRotateNm::MatrixInCircle objMatrixInCircle;
 	objMatrixInCircle.callMatrixInCircle();
 
-	SetComparisonNM::testSetComparisonNM objtestSetComparisonNM;
-	objtestSetComparisonNM.SetComparisonNM();
 
-	MapComparisonNM::testMapComparisonNM  objtestMapComparisonNM;
-	objtestMapComparisonNM.MapComparisonNM();
 
-	UsageOfPrioriQNm::TestUsageOfPriorotyQ objTestUsageOfPriorotyQ;
-	objTestUsageOfPriorotyQ.callTestingUsageOfPriorityQ();
 
 	editablePQ::TestEditablePQ objTestEditablePQ;
 	objTestEditablePQ.callEditablePQ();
@@ -44,8 +137,6 @@ int main()
 	RemoveCommaX objRemoveCommaX;
 	objRemoveCommaX.callRemoveComma();
 
-	BinSearchForLoop objBinSearchForLoop;
-	objBinSearchForLoop.callBinSearchForLoop();
 
 	print123withPlusX objprint123withPlusX;
 	objprint123withPlusX.callprint123withPlusX();
