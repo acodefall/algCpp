fd4fdf7eabad1588a1e8ec6497745c9764221a6e
acodefall <asnowfall@hotmail.com>
2017-08-23 03:07:00 -0400

    23

--- algCpp/algCpp/Array/EncodeReplaceMultipleWithNumber.h
+++ algCpp/algCpp/Array/EncodeReplaceMultipleWithNumber.h
@@ -10,39 +10,73 @@
 #include <sstream>
 
 using namespace std;
-namespace EncodeReplaceMultipleWithNumberNM
+namespace EncodeReplaceMultipleWithNumberNM //@RED20170823001
 {
 	/*
-		Encode the string by replacing the repeating letters with their occurence-count followed by character itself.
+		Encode the string by replacing the repeating character with occurence-count
 		Ex: Replace "CC" using "2C"
-		Get occurenace count using look forward strategy.  Look forward strategy compares the current value 
-		against the next-value and next-value; and this continues as long as value is matching.
-		This will give the repeat count of current-value. If the item is non-repeating then occurent count is 1.
-		Once we have the occurent count, decided whether to encode or not. If we decide to encode, string may shrink, in that case increment
-		the 'shiftCount' also. 
-		Say occurent count is 1, then no encoding is needed, copy the curr-value to appropriate index.
-		Say occurent count is 2 or more, then encode the character. (like AA 2A). Also compute the shift count.
-		"shift count = shift count + occurent count - 2" //Two accounts for "2A"
+			Scan the array and detect how many times a character repeats. Use the Look forward strategy for detecting occurrence count. Look forward strategy uses a while loop; before entering the while loop, cache the current-character, and once inside the while loop, go on comparing the curr-character against cached character. If they matach, then increment the occurent count, and also advance the index, and also stay in the loop. Come out of the loop once curr-character is not sam as cached character.
+			Once out of inner while loop, we know occurent count. It could be 1 or more. 1 means character is non-repeating, so ecoding is needed; otherwise we have to encode. Now copy the characters to appropriate position. 
 
-		There will be outer-while loop and inner loop. Inner while loop is used for Forward lookup.
-		Both use different index. If Outer while loop(I) is at 2, then inner while loop starts from index-3.
-		Before getting in to inner-while loop, initialize the  'occurence count' = 1 because item is already existing once.
-		Once we come out of this while loop, we will know the occurence count.
-		USe iToA() function for converting 'occurent count' in to number. This is because occurent count could be 2 or 200
-		while(i < Len)
-		{
-			i = j + 1;
-			occurCount = 1;
-			//Look forward loop
-			while(j < Len)
+			After copying is done we might have SHRUUNK the arrasy so we have to compute the "shift count .
+			So "shift count = shift count + occurent count - leghtOfNumeralPrefix" //leghtOfNumeralPrefix could be multi-digit. 2A or 20A
+
+			Coming to implementation, there will be outer-while loop and inner loop.	Both use different index variables; but inner while loop starts after the outer-loop’s index. If Outer while loop(I) is at 2, then inner while loop starts from index-3.
+			Before getting in to inner-while loop, initialize the  'occurence count' = 1 because current-character cached  character has already appeared once.
+
+			while(i < Len)
 			{
-				occurCount++;
+				c = src[i];                                  
+				i = j + 1;                                    
+				occurCount = 1;                              
+		
+				while(j < Len) 
+				{
+					if(cachedChar = src[j]
+					   occurCount++;
+					 else
+						break;
+				}
+														
+				if(occurCount == 1) 
+					src[(i - shiftCount)] = cache current character;
+				else 
+					itoA()
+					Write COUNT & Alphabet That is 2A
+					Array has shrunk because AAA became 3A, so adjust shiftCount  
+					shiftCount = shiftCount + occurcount - (countlen + 1);
 			}
 
-			if(occurCount == 1) //Non-repeating character
-			else
-				//Repeating character
-		}
+			"u"
+				-occurCount is 1
+				-directly breaks;
+				-No encoding, direct copy, no shiftCount modification
+
+
+			"ss"
+				occurCount is 2
+				Make AAA in to 3A
+				Adjust shiftCount
+
+
+		Decode the string like "2A"
+			Encoded string may have "2A" and this should be decoded as AA.
+			Numeral prefix could be multidigit, so use forward looking strategy for capturing digits length.
+			At every index, kick start a inner while loop for counting digit length.While loop tests whether the curr-char is Numeral and if so it increments the numeral-counter, and continues to test the next-character, and then to next-to-next-character. This goes on until the sequence of numerals breaks. Loop starts at the same index as outer while loop.
+			Once we are out of the while loop, we will have the numeral-count, and this tells how many number of alphabets to
+			be copied to out[]. Cursor will be pointing to the character after the Numeral.
+    
+			 -if the ‘numeral-count' is 0, there was no numeral prefix and character repeats only once. 
+				 So copy the current character to out[] as it is.
+				 Ex: For "ABC", 'number lengh' will be 0.
+
+				-if the '‘numeral-count'' > 0, then character repeats several times and do Atoi() conversion.
+				 Then copy the multiple instances of current character in to out[].
+				  Ex: For "3ABC", 'number lengh' will be 3. AAABC
+    
+			Coming to implementation, there will be outer while loop and inner while loop, and  both will use diifferent indexes variables.  Inner while loop start at the same index as outer while loop. If outer loop at [5] and inner while loop will also start at [5] and goes until it finds a non-numeral.
+
+			
 
 	*/
 	class EncodeReplaceMultipleWithNumber
@@ -52,8 +86,76 @@ namespace EncodeReplaceMultipleWithNumberNM
 				{
 					string src("ussfreeet"); ///u2sfr3et
 					string encodedSrc = EncodeReplaceMultipleWithNumberX(src);
-				}
 
+					cout << "src '"<< src <<"'" << endl;
+					cout << "encoded '"<< encodedSrc << "'" << endl;
+					string DecodedSrc = Decode(encodedSrc);
+					cout << "Decoded '" << DecodedSrc << "'" << endl;
+				}
+				string Decode(string srcIn)
+				{
+					string outDecString;
+					char* src = (char*)srcIn.c_str();
+					int i = 0;
+
+					
+					while (i < srcIn.length())
+					{
+						int j = i; //Inner while loop start at the same index as outer while loop.
+						int numberLen = 0;
+						
+						//While loop tests whether the curr-value is Numeral and if so increments the counter(numberLen), and continues to test next 
+						//index, and to next-to-next-index. Loop goes from current index(same as outer while loop) and stops when 
+						//curr - value becomes non - numeral(alphabet)..
+						while (j < srcIn.length())
+						{
+							if (src[j] >= '0' && src[j] <= '9')
+							{
+								numberLen++;
+							}
+							else
+							{
+								break;
+							}
+							j++;
+						}
+						
+						if (numberLen > 0)
+						{//'number lengh' > 0, then number repeats several times and we have to do Atoi() conversion.
+							//Then copy the multiple instances of current character in to out[].
+							//	Ex: For "3ABC", 'number lengh' will be 3. AAABC
+
+							//read the character that is after the number Eg: 2A
+							char letter;
+							if (j < srcIn.length())
+								letter = src[j];
+							
+							//convert the 'number lengh' in to INT
+							string temp = srcIn.substr(i, numberLen);
+							int charLen = std::atoi(temp.c_str());
+							
+							//Copy the multiple instances of current character in to out[].
+							while (charLen > 0)
+							{
+								outDecString += letter;
+								charLen--;
+							}
+
+							//"2Abc"
+							//advance the i. Now j will be pointing A
+							i = j;
+						}
+						else
+						{
+							//'number lengh' is 0, then character is alphabet, it repeats only once.
+							//	So copy the current character to out[] as it is.
+							//	Ex: For "ABC", 'number lengh' will be 0.
+							outDecString += src[i];
+						}
+						i++;
+					}
+					return outDecString;
+				}
 				string EncodeReplaceMultipleWithNumberX(string srcIn)
 				{
 					char* src = (char*)srcIn.c_str();
@@ -89,14 +191,18 @@ namespace EncodeReplaceMultipleWithNumberNM
 							char num[5] = { 0 };
 							itoa(occurcount, num, 10);// 
 							int countlen = strlen(num);
+
+							//Copy the counter
 							for (int k = 0; k < countlen; k++)
 							{
 								src[(i - shiftCount) + k] = num[k];
 							}
+
+							//copy the Alhabet
 							src[(i - shiftCount) + countlen] = c;
 							
-							//adjust the shiftcount
-							shiftCount = shiftCount + occurcount - (countlen + 1);//subtract the space taken by "2A"
+							//Array has shrunk because AAA became 3A, so adjust shiftCount  
+							shiftCount = shiftCount + occurcount - (countlen + 1); //countlen "1 for 2A"; "2 for 45A";
 							
 							i += (occurcount - 1); //Use '-1' because occurcount is COUNT and i is INDEX
 						}
